c337677084ebd42e980a6b643e6711d521effa42
DasHeiligeDoenerhuhn <93918690+DasHeiligeDoenerhuhn@users.noreply.github.com>
2022-02-13 10:29:29 +0100

    Slight GUI changes

--- WindowsFormsApp8/Form1.Designer.cs
+++ WindowsFormsApp8/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp8
 {
-    partial class Form1
+    partial class form1
     {
         /// <summary>
         /// Erforderliche Designervariable.
@@ -28,11 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form1));
             this.button1 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.richTextBox3 = new System.Windows.Forms.RichTextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -47,29 +51,29 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(13, 58);
+            this.richTextBox1.Location = new System.Drawing.Point(109, 57);
             this.richTextBox1.MaxLength = 8;
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(140, 25);
+            this.richTextBox1.Size = new System.Drawing.Size(75, 25);
             this.richTextBox1.TabIndex = 3;
-            this.richTextBox1.Text = "Filename";
+            this.richTextBox1.Text = "MyFile";
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(170, 58);
+            this.richTextBox2.Location = new System.Drawing.Point(279, 57);
             this.richTextBox2.MaxLength = 8;
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(134, 24);
+            this.richTextBox2.Size = new System.Drawing.Size(77, 24);
             this.richTextBox2.TabIndex = 4;
-            this.richTextBox2.Text = "Foldername";
+            this.richTextBox2.Text = "main";
             // 
             // richTextBox3
             // 
-            this.richTextBox3.Location = new System.Drawing.Point(13, 90);
+            this.richTextBox3.Location = new System.Drawing.Point(13, 119);
             this.richTextBox3.Name = "richTextBox3";
-            this.richTextBox3.Size = new System.Drawing.Size(775, 348);
+            this.richTextBox3.Size = new System.Drawing.Size(775, 319);
             this.richTextBox3.TabIndex = 5;
-            this.richTextBox3.Text = "Filecontent";
+            this.richTextBox3.Text = resources.GetString("richTextBox3.Text");
             // 
             // button2
             // 
@@ -81,19 +85,50 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // Form1
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Filename";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(190, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Foldername";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "File Content";
+            // 
+            // form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.richTextBox3);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "form1";
+            this.Text = "XCP-Editor";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -104,6 +139,9 @@
         private System.Windows.Forms.RichTextBox richTextBox2;
         private System.Windows.Forms.RichTextBox richTextBox3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- WindowsFormsApp8/Form1.cs
+++ WindowsFormsApp8/Form1.cs
@@ -11,9 +11,9 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp8
 {
-    public partial class Form1 : Form
+    public partial class form1 : Form
     {
-        public Form1()
+        public form1()
         {
             InitializeComponent();
         }
